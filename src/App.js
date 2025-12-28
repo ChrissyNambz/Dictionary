cd1ecc71c99dbd9c150d62b07721cfd7f9bbacad
ChrissyNambz <christine.nambatya@gmail.com>
2025-12-28 22:45:42 +0000

    Created new component
    -Created a simple search engine for the dictionary
    - can search for the word & receive  response "searching response from useState

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import "./App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookBookmark } from "@fortawesome/free-solid-svg-icons";
+import Dictionary from "./Dictionary"
 
 
 function App() {
   return (
     <div className="App">
+     <div className="container">
       <header className="App-header">
         <FontAwesomeIcon icon={faBookBookmark} size="2x" />
         <h1>Dictionary App</h1>
       </header>
+      <main>
+        <Dictionary/>
+      </main>
+      <footer className="footer"> Coded by Christine Nambatya 🐾</footer>
+      </div>
     </div>
   );
 }
-
+ 
 export default App;
