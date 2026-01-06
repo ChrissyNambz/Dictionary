fed636b02e143fafa05883383b444dd0a553c13d
ChrissyNambz <christine.nambatya@gmail.com>
2026-01-06 00:24:28 +0000

    Added images using API for dictionary app

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 import React, {useState}from "react";
 import axios from "axios"
 import Results from "./Results"
+import Photos from "./Photos";
 import "./Dictionary.css"
 
 export default function Dictionary(props){
 let [keyword, setKeyword]=useState(props.defaultKeyword);
 let [results, setResults]=useState(null);
 let [loaded, setLoaded]=useState(false);
+let [photos, setPhotos]=useState(null)
 
  function handleResponse(response){
     setResults(response.data[0])
@@ -14,9 +16,21 @@ let [loaded, setLoaded]=useState(false);
 
  }
 
+  function handlePexResponse(response){
+    setPhotos(response.data.photos);
+
+  }
+
 function search(){
   let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
   axios.get(apiUrl).then(handleResponse);
+
+ let pexApiKey = "hOvjf20B4f3mkVMKLnyxoIINQfAHQhaFAFcpqgBvslxVHPIzmLTA92ut";
+  let pexApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+  let headers = { Authorization: `Bearer ${pexApiKey}` };
+axios
+  .get(pexApiUrl, {headers: headers })
+  .then(handlePexResponse);
 }
 
 
@@ -53,6 +67,7 @@ if (loaded){
        </div>
      </section>
      <Results results={results} />
+     <Photos photos={photos}/>
    </div>
  );
 
